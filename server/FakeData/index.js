8ac015d2211666d7b92631076bfbcb8bbf750285
CkiucuaDaoDao <vinhquangtran1820@gmail.com>
2023-08-05 16:54:05 +0700

    Note-App

---
+++ server/FakeData/index.js
@@ -0,0 +1,28 @@
+export default {
+    authors: [
+        {
+            id: 123,
+            name: 'Quang Vinh'
+        },
+    ],
+    folders: [
+        {
+            id: 1,
+            name: 'Home',
+            createdAt: '2023',
+            authorId: 123,
+        },
+        {
+            id: 1,
+            name: 'Home',
+            createdAt: '2023',
+            authorId: 123,
+        },
+        {
+            id: 1,
+            name: 'Home',
+            createdAt: '2023',
+            authorId: 123,
+        }
+    ]
+}
